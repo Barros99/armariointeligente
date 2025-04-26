3f3ab269965c2608889985a3ba4cb113ad28164d
Jean Meneses Barros <jeanmeneses@hotmail.com>
2025-04-26 00:16:41 -0300

    refactor(TipoUsuarioController): melhora o tratamento de exceções
    remove blocos try-catch no controlador e lança ResourceNotFoundException quando um TipoUsuario não é encontrado. Isso simplifica a lógica do controlador e centraliza o tratamento de exceções.

--- src/main/java/br/com/unit/tokseg/armariointeligente/controller/TipoUsuarioController.java
+++ src/main/java/br/com/unit/tokseg/armariointeligente/controller/TipoUsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.unit.tokseg.armariointeligente.exception.ResourceNotFoundException;
 import br.com.unit.tokseg.armariointeligente.model.TipoUsuario;
 import br.com.unit.tokseg.armariointeligente.service.TipoUsuarioService;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,58 +25,37 @@ public class TipoUsuarioController {
 
     @PostMapping
     public ResponseEntity<?> criarTipoUsuario(@RequestBody TipoUsuario tipoUsuario) {
-        try {
-            TipoUsuario novoTipoUsuario = tipoUsuarioService.criarTipoUsuario(tipoUsuario);
-            return ResponseEntity.ok(novoTipoUsuario);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        TipoUsuario novoTipoUsuario = tipoUsuarioService.criarTipoUsuario(tipoUsuario);
+        return ResponseEntity.ok(novoTipoUsuario);
     }
 
     @GetMapping
     public ResponseEntity<?> listarTiposUsuarios() {
-        try {
-            return ResponseEntity.ok(tipoUsuarioService.listarTiposUsuarios());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok(tipoUsuarioService.listarTiposUsuarios());
     }
 
 
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarTipoUsuarioPorId(@PathVariable Long id) {
-        try {
-            Optional<TipoUsuario> tipoUsuario = tipoUsuarioService.buscarTipoUsuarioPorId(id);
-            if (tipoUsuario.isPresent()) {
-                return ResponseEntity.ok(tipoUsuario.get());
-            } else {
-                return ResponseEntity.status(404)
-                        .body("Tipo de usuário não encontrado com o ID: " + id);
-            }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        Optional<TipoUsuario> tipoUsuario = tipoUsuarioService.buscarTipoUsuarioPorId(id);
+        if (tipoUsuario.isPresent()) {
+            return ResponseEntity.ok(tipoUsuario.get());
+        } else {
+            throw new ResourceNotFoundException("Tipo de usuário", "id", id);
         }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> atualizarTipoUsuario(@PathVariable Long id,
             @RequestBody TipoUsuario tipoUsuario) {
-        try {
-            TipoUsuario tipoAtualizado = tipoUsuarioService.atualizarTipoUsuario(id, tipoUsuario);
-            return ResponseEntity.ok(tipoAtualizado);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        TipoUsuario tipoAtualizado = tipoUsuarioService.atualizarTipoUsuario(id, tipoUsuario);
+        return ResponseEntity.ok(tipoAtualizado);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletarTipoUsuario(@PathVariable Long id) {
-        try {
-            tipoUsuarioService.deletarTipoUsuario(id);
-            return ResponseEntity.noContent().build();
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        tipoUsuarioService.deletarTipoUsuario(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
